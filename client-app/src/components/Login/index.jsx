81cff5a76a54c7a554c66f7427367f15e5b7e545
phuocleoceo <ht10082001@gmail.com>
2022-01-08 13:46:14 +0700

    Login

--- client-app/src/components/Login/index.jsx
+++ client-app/src/components/Login/index.jsx
@@ -1,20 +1,47 @@
+import { LoginAction } from '../../redux/slices/authenticationSlice';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 import React from 'react';
 
 export default function Login()
 {
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = async (data) =>
+    {
+        const check = await dispatch(LoginAction(data));
+        if (check.payload.Accepted)
+        {
+            toast.success("Login Successfully");
+            navigate("/");
+        }
+        else
+        {
+            toast.error("Login Failure");
+        }
+    };
+
     return (
         <div className="login-form-container">
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <h3>LOGIN FORM</h3>
-                <input type="text" name="" placeholder="Enter your username..." className="box" />
-                <input type="password" name="" placeholder="Enter your password..." className="box" />
+                <input type="text" {...register("username")} required
+                    placeholder="Enter your username..." className="box" />
+
+                <input type="password" {...register("password")} required
+                    placeholder="Enter your password..." className="box" />
+
                 <div className="remember">
                     <input type="checkbox" name="" id="remember-me" />
-                    <label>remember me</label>
+                    <label>Remember Me</label>
                 </div>
-                <input type="submit" value="login now" className="btn" />
-                <p>forget password? <a href="/">click here</a></p>
-                <p>don't have an account? <a href="/">create one</a></p>
+                <input type="submit" value="Login Now" className="btn" />
+                <p>Forget Password? <a href="/">Click Here</a></p>
+                <p>Don't Have An Account? <a href="/">Create One</a></p>
             </form>
 
         </div>
--- client-app/src/components/Navigation/index.jsx
+++ client-app/src/components/Navigation/index.jsx
@@ -1,10 +1,21 @@
-import { useSelector } from 'react-redux';
-import { Link } from "react-router-dom";
+import { Logout } from '../../redux/slices/authenticationSlice';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useNavigate } from "react-router-dom";
 import React from 'react';
 
 export default function Navigation()
 {
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
+    const user = useSelector(state => state.authentication);
+
+    const handleLogout = () =>
+    {
+        dispatch(Logout());
+        navigate("/");
+    };
+
     return (
         <header className="header">
             <Link to="/">
@@ -16,25 +27,32 @@ export default function Navigation()
             <nav className="navbar">
                 <Link to="/"><div className="link">Trang chủ</div></Link>
                 <Link to="/menu"><div className="link">Menu</div></Link>
-                <Link to="/manage"><div className="link">Quản lý</div></Link>
+                {
+                    user.role === "admin" &&
+                    <Link to="/manage"><div className="link">Quản lý</div></Link>
+                }
             </nav>
 
             <div className="icons">
-                <span style={{ fontSize: 20 }}>Admin</span>
-                <div id="menu-btn" className="fas fa-bars"></div>
-                <Link to="/cart">
-                    <div id="cart-btn" className="fas fa-shopping-cart">
-                        <span style={{ color: "red", fontSize: 17 }}>
-                            {cart.count > 0 ? cart.count : "0"}
-                        </span>
-                    </div>
-                </Link>
-                <Link to="/login">
-                    <div id="login-btn" className="fas fa-user"></div>
-                </Link>
-                <Link to="/logout">
-                    <div id="logout-btn" className="fas fa-sign-out-alt"></div>
-                </Link>
+                {
+                    user.id ?
+                        <>
+                            <span style={{ fontSize: 20 }}>{user.displayname}</span>
+                            <div id="menu-btn" className="fas fa-bars"></div>
+                            <Link to="/cart">
+                                <div id="cart-btn" className="fas fa-shopping-cart">
+                                    <span style={{ color: "red", fontSize: 17 }}>
+                                        {cart.count > 0 ? cart.count : "0"}
+                                    </span>
+                                </div>
+                            </Link>
+                            <div id="logout-btn" onClick={handleLogout} className="fas fa-sign-out-alt"></div>
+                        </>
+                        :
+                        <Link to="/login">
+                            <div id="login-btn" className="fas fa-user"></div>
+                        </Link>
+                }
             </div>
         </header>
     )
