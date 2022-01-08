d36765ef36385fa868ccdfa097be9e3a7bb6c3d3
phuocleoceo <ht10082001@gmail.com>
2022-01-08 10:52:30 +0700

    backup Cart

--- client-app/src/components/Cart/index.jsx
+++ client-app/src/components/Cart/index.jsx
@@ -1,26 +1,56 @@
 import { INCREASE_QUANTITY, DECREASE_QUANTITY, REMOVE_CART } from '../../redux/slices/cartSlice';
 import { useSelector, useDispatch } from 'react-redux';
-import React from 'react';
+import { GET_LIST } from '../../api/apiFood';
+import React, { useState, useEffect } from 'react';
 
 export default function Cart()
 {
     const dispatch = useDispatch();
     const cart = useSelector(state => state.cart);
+    const [food, setFood] = useState([]);
+    const [total, setTotal] = useState(0);
 
-    const handleMinus = async (foodId) =>
+    useEffect(() =>
     {
-        await dispatch(DECREASE_QUANTITY(foodId));
-    }
+        const getFoodInCart = async () =>
+        {
+            if (cart.count > 0)
+            {
+                let listId = "";
+                cart.foodInCart.forEach(food => listId += food.id + ",");
+                // Remove "," at last
+                listId = listId.slice(0, -1);
 
-    const handlePlus = async (foodId) =>
-    {
-        await dispatch(INCREASE_QUANTITY(foodId));
-    }
+                const response = await GET_LIST(listId);
+                const foodList = response.data.map(f => (
+                    {
+                        ...f,
+                        quantity: cart.foodInCart.find(c => c.id === f.id).quantity
+                    }
+                ));
+                setFood(foodList);
+            }
+        };
+        getFoodInCart();
+    }, [cart]);
 
-    const handleRemove = async (foodId) =>
+    useEffect(() =>
     {
-        await dispatch(REMOVE_CART(foodId));
-    }
+        let sum = 0;
+        food.forEach(f =>
+        {
+            sum += f.price * f.quantity;
+        });
+        setTotal(sum);
+    }, [food]);
+
+    const handleMinus = (foodId) => dispatch(DECREASE_QUANTITY(foodId));
+
+
+    const handlePlus = (foodId) => dispatch(INCREASE_QUANTITY(foodId));
+
+
+    const handleRemove = (foodId) => dispatch(REMOVE_CART(foodId));
 
     return (
         <section className="shopping-cart-container">
@@ -28,20 +58,20 @@ export default function Cart()
                 <h3 className="title">Giỏ hàng</h3>
                 <div className="box-container">
                     {
-                        cart.count > 0 &&
-                        cart.foodInCart.map(c => (
-                            <div className="box" key={c.id}>
-                                <i className="fas fa-times" onClick={() => handleRemove(c.id)}></i>
-                                <img src="https://mcdn2-coolmate.cdn.vccloud.vn/uploads/October2021/meme-cheems-25.jpg" alt="" />
+                        food.length > 0 &&
+                        food.map(f => (
+                            <div className="box" key={f.id}>
+                                <i className="fas fa-times" onClick={() => handleRemove(f.id)}></i>
+                                <img src={f.imagePath} alt="" />
                                 <div className="content">
-                                    <h3>ahihi</h3>
+                                    <h3>{f.name}</h3>
                                     <span> Số lượng : </span> <br />
-                                    <button className="btnQuantity" onClick={() => handleMinus(c.id)}>-</button>
-                                    <span className="quantity">{c.quantity}</span>
-                                    <button className="btnQuantity" onClick={() => handlePlus(c.id)}>+</button>
+                                    <button className="btnQuantity" onClick={() => handleMinus(f.id)}>-</button>
+                                    <span className="quantity">{f.quantity}</span>
+                                    <button className="btnQuantity" onClick={() => handlePlus(f.id)}>+</button>
                                     <br />
                                     <span> Giá : </span>
-                                    <span className="price"> 10000 </span>
+                                    <span className="price"> {f.price} VNĐ</span>
                                 </div>
                             </div>
                         ))
@@ -52,7 +82,7 @@ export default function Cart()
             <div className="cart-total">
                 <h3 className="title"> Tổng cộng </h3>
                 <div className="box">
-                    <h3 className="total"> Số tiền : <span>200000 VNĐ</span> </h3>
+                    <h3 className="total"> Số tiền : <span>{total} VNĐ</span> </h3>
                     <button className="btn">Xác nhận đặt món</button>
                 </div>
             </div>
